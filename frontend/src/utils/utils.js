87f1767914b01b40e0f92b13d40d9ad5384acb69
Iurii Bairamukov <juribai@mail.ru>
2020-11-28 11:30:14 +0700

    feat: добавить папку с фронтендом

---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,9 @@
+export const BASE_URL =
+  'https://api.bairamukov-mesto.students.nomoreparties.space';
+
+export const configHeaders = {
+  baseUrl: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
